92b8a5c887b83de07f5800ce89c26e3fb720ea01
Jon Wood <jon@blankpad.net>
2018-04-28 14:55:09 +0100

    Screen filling thingy

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,14 +1,48 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+// Address of last block of pixels.
+@24576
+D=A
+@MAX
+M=D
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+(RESET)
+  @SCREEN
+  D=A
+  @NEXT
+  M=D
 
-// Put your code here.
+(LOOP)
+  @KBD
+  D=M
+
+  @WHITE
+  D;JEQ
+
+  // Set the next block to fully black
+  (BLACK)
+  @0
+  D=A-1
+  @FILL
+  0;JMP
+
+  (WHITE)
+  @0
+  D=A
+
+  (FILL)
+  @NEXT
+  A=M
+  M=D
+  // Set the value of @NEXT
+  D=A+1
+  @NEXT
+  M=D
+
+  // Move to the next pixel, unless we've run
+  // out of screen.
+  @MAX
+  D=D-M
+  @RESET
+  D;JEQ
+
+  @LOOP
+  0;JMP
